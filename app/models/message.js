67dd0d205c0d48980b7ae55058763a561d138a8c
jasmeen33 <81803811+jasmeen33@users.noreply.github.com>
2021-05-19 13:16:44 +0530

    Add files via upload

---
+++ app/models/message.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiverId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  message: {
+    type: String
+  },
+  sendDate: {
+    type: Date,
+    default: Date.now
+  },
+  readed: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
