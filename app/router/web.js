c4897ae9934991c9b6e7dc3681a3b035be1fda83
jasmeen <jasmeen@chicmic.co.in>
2021-05-20 14:38:07 +0530

    socket commit

--- app/router/web.js
+++ app/router/web.js
@@ -1,11 +1,16 @@
 const userController = require('../controller/userConroller')
 const auth = require('../middleware/auth')
+const cors = require('cors');
 const routerInit =(app)=>{
- 
-    app.get('/',userController().index)
-
+app.use(cors());
+    
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
     app.get('/userprofile',auth,userController().profile)
-    app.get('/getusers',auth,userController().allUser)
+    app.get('/getusers',userController().allUser)
 
     app.get('/userlogout',auth,userController().logout)
 
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
+const express = require('express'), http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server,{cors: {
+    origin: '*',
+  }});
+
 
 const port = 8000
 const url = 'mongodb://127.0.0.1:27017/chatapplication';
+let connectedUsers=[];
+let usersConnected = {}
 
-//set application to accept the form data
 // app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
@@ -14,13 +21,55 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCrea
     console.log("connecting succesfully...")
 }).catch((err) => console.log(err))
 
-
-
 const routerInit = require('./app/router/web')
-routerInit(app)
+routerInit(app);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect',()=>{
+        console.log("a user disconnected");
+        connectedUsers = connectedUsers.filter(
+            (item)=>{
+                item.socketId != socket.id;
+            }
+        );
+        Object.keys(usersConnected).forEach(key => {
+            if (usersConnected[key] == socket.id) delete usersConnected[key];
+        })
+        io.emit('updateUserList',connectedUsers);
+    });
+    socket.on('loggedin',(user)=>{
+        connectedUsers.push({...user})
+        usersConnected[user._id] = socket.id.toString();
+	
+        console.log(connectedUsers);
+        // let users = connectedUsers.filter(
+        //     item =>{
+        //         item._id != user._id
+        //     }
+        // )
+        io.emit('updateUserList',connectedUsers);
+    })
+    socket.on('chatMessage', function(data){
+        console.log(data.receiver)
+        // user = connectedUsers.filter(
+        //     (item)=>{
+        //         if(item._id == data.receiver){
+        //             console.log(item.socketId);
+        //             socket.to(item.socketId).emit('message', data);
+        //             console.log(data.message)
+        //         }
+        //     }
+        // );
+        socket.to(usersConnected[data.receiver]).emit('message', data);
+     });
+    
+  });
 
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`app running on the port: ${port}`)
 })
+
+
